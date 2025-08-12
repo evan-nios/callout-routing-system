a300a06489b6a9dbf72709a38ef8d92992e9d8e7
evan-nios <evan@niosspa.com>
2025-08-12 10:05:37 -0400

    Update routing_system.py
    Update Josh Manager Schedule

--- routing_system.py
+++ routing_system.py
@@ -11,10 +11,10 @@ class CallOutRoutingSystem:
             'Phone Number': ['+13473150211', '+13477168143', '+19175197053'],
             'Sunday': ['-', '8:30 AM - 8:00 PM', '-'],
             'Monday': ['-', '8:30 AM - 8:00 PM', '8:30 AM - 4:00 PM'],
-            'Tuesday': ['12:00 PM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 4:00 PM'],
-            'Wednesday': ['8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 4:00 PM'],
-            'Thursday': ['8:30 AM - 8:00 PM', '12:00 PM - 8:00 PM', '8:30 AM - 4:00 PM'],
-            'Friday': ['8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 4:00 PM'],
+            'Tuesday': ['12:00 PM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM'],
+            'Wednesday': ['8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM'],
+            'Thursday': ['8:30 AM - 8:00 PM', '12:00 PM - 8:00 PM', '8:30 AM - 8:00 PM'],
+            'Friday': ['8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM', '8:30 AM - 8:00 PM'],
             'Saturday': ['10:45 AM - 8:00 PM', '-', '-']
         }
         
